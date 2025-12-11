5f14cdf330da9c9a46e48329759670c1e7f7f3ca
Luisa-039 <168785270+Luisa-039@users.noreply.github.com>
2025-12-10 19:25:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://avisena-frontend.vercel.app",
+        "https://backend-avisena.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],  # Permitir cualquier encabezado en las solicitudes
